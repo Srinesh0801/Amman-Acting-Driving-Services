0b1e22387b6c4dde0e39fe872b6d6c70bb99bbde
Srinesh0801 <srinesh0801@gmail.com>
2025-12-19 07:36:39 +0000

    Updated UI / fixed bug / new feature

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -541,11 +541,7 @@ const Index = () => {
       <span>+91 9080512549 &nbsp;|&nbsp; +91 9940990684</span>
     </p>
     <p className="flex justify-center items-center gap-2 text-foreground">
-    git status
-git add .
-git commit -m "Updated UI / fixed bug / new feature"
-git push origin main
- */}
+      {/* <Mail className="w-5 h-5 text-accent" /> */}
       {/* <a
         href="mailto:info@ammanactive.com"
         className="hover:text-accent transition-colors"
